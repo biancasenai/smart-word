2ed2675ecd560de5847741d7d58a811798d9826a
Bianca <bianca.furlanetto440@gmail.com>
2025-12-09 16:13:40 -0300

    refactor: reorganize components and update routing for user authentication

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { Routes, Route, useNavigate } from "react-router";
 import "./App.css";
-import Cadastro from "./componentes/Cadastro.jsx";
-import Login from "./componentes/login";
 import Home from "./pages/home";
 import Bateria from "./pages/bateria";
 import Relatorio from "./pages/Relatório";
@@ -18,6 +16,8 @@ import logoLight from "./img/logoligth.png";
 import logoDark from "./img/logoDark.png";
 import ChatBot from "./pages/ChatBot";
 import { ThemeProvider, UseTheme } from "./componentes/ThemeContext";
+import Cadastro from "./pages/Cadastro.jsx";
+import Login from "./pages/login.jsx";
 
 // Botão voltar para todas as páginas
 function VoltarHomeButton() {
@@ -29,11 +29,14 @@ function VoltarHomeButton() {
 }
 
 function App() {
-  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showLogin, setShowLogin] = useState(false); // alterna entre cadastro e login
-  const [showCadastro, setShowCadastro] = useState(false); // alterna entre cadastro e login
+  const [showCadastro, setShowCadastro] = useState(false);
+  const navigate = useNavigate();
   const { darkMode, toggleTheme } = UseTheme(); // Usa o contexto do tema
+
+  const goToCadastro = () => {
+    navigate("/cadastro");
+  };
 
   // Escolhe a imagem conforme o tema
   const logo = darkMode ? logoDark : logoLight;
@@ -138,6 +141,8 @@ function App() {
             <Route path="/pontosHoteis" element={<PontosHoteis />} />
             <Route path="/pontosCashback" element={<PontosCashback />} />
             <Route path="/pontosProdutos" element={<PontosProdutos />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
             <Route
               path="/chatbot"
               element={
@@ -153,9 +158,10 @@ function App() {
           onCadastro={() => setIsLoggedIn(true)} // depois do cadastro vai pra Home
         />
       ) : (
-        <Login
-          onLogin={() => setIsLoggedIn(true)}
-        />
+        <>
+          <Login onLogin={() => setIsLoggedIn(true)} />
+          <button onClick={goToCadastro}>Cadastrar-se</button>
+        </>
       )}
     </div>
   );
